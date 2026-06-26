88c22511fbdfff45ccd3b2409ca8a40d6d19d26a
JuzerL <juzerrezuj@gmail.com>
2026-06-26 14:11:19 +0530

    Update datapath.v

--- datapath.v
+++ datapath.v
@@ -1,9 +1,9 @@
 `timescale 1ns / 1ps
 
 module datapath(
-    input clk, reset, pc_write_enable, reg_write_enable,mem_to_reg,mem_write_enable,
+    input clk, reset, pc_write_enable, reg_write_enable,mem_to_reg,mem_write_enable,alu_src,
     input ir_write_1,ir_write_2,ir_write_3,
-    input branch_enable,
+    input [1:0] pc_src, 
     input [2:0] alu_control,
     output sign_flag, zero_flag,
     output [23:0] instruction
@@ -16,12 +16,49 @@ module datapath(
         wire [7:0] mem_read_data;
         wire [7:0] rom_data;
         wire [7:0] pc_in;
+        wire [7:0] imm;
+        wire [7:0] actual_alu_input_b;
+        wire [7:0] branch_target;
         
         assign write_data = (mem_to_reg == 1'b1) ? mem_read_data : alu_result;
-        assign pc_in = (branch_enable == 1'b1) ? alu_result : (pc_instmem + 8'd1);
-        assign rs1_addr = instruction[15:13];
-        assign rs2_addr = instruction[12:10];
-        assign rd_addr  = instruction[18:16];
+        assign branch_target = pc_instmem + imm;
+        
+        assign pc_in = (pc_src == 2'b11) ? imm :
+                       (pc_src == 2'b10) ? rs1_data :       
+                       (pc_src == 2'b01) ? branch_target :        
+                       (pc_instmem + 8'd1);                 
+                       
+        reg [2:0] decoded_rs1;
+reg [2:0] decoded_rs2;
+
+
+always @(*) begin
+
+    decoded_rs1 = instruction[15:13]; 
+    decoded_rs2 = instruction[12:10];
+
+    case(instruction[23:19]) 
+        5'b00100, 
+        5'b00110, 
+        5'b01110: decoded_rs1 = instruction[18:16];
+         
+        5'b00010,
+        5'b10000, 
+        5'b10001, 
+        5'b10010: 
+        begin
+            decoded_rs1 = instruction[18:16];
+            decoded_rs2 = instruction[15:13];
+        end
+    endcase
+end
+
+assign rs1_addr = decoded_rs1;
+assign rs2_addr = decoded_rs2;
+
+assign rd_addr  = instruction[18:16];
+assign imm = instruction[7:0];
+assign actual_alu_input_b = alu_src ? imm : rs2_data; 
 
     pc pcount(
     .clk(clk), .reset(reset), .pc_write_enable(pc_write_enable), .pc_in(pc_in), .pc_out(pc_instmem)
@@ -37,7 +74,7 @@ module datapath(
     );
     
     alu aloo(
-        .input_A(rs1_data), .input_B(rs2_data), .alu_result(alu_result),
+        .input_A(rs1_data), .input_B(actual_alu_input_b), .alu_result(alu_result),
          .sign_flag(sign_flag), .zero_flag(zero_flag), .alu_control(alu_control)
     );
     
