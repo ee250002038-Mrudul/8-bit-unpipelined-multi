be3997e44a5c6dcdf2cee9f7dcfecaf543b5bba8
JuzerL <juzerrezuj@gmail.com>
2026-06-26 14:12:03 +0530

    Update cpu_top.v

--- cpu_top.v
+++ cpu_top.v
@@ -6,16 +6,17 @@ module cpu(
 );
 
     wire [23:0] instruction;
-    wire pc_write_enable, reg_write_enable, mem_write_enable, mem_to_reg, branch_enable;
+    wire pc_write_enable, reg_write_enable, mem_write_enable, mem_to_reg, alu_src;
+    wire [1:0] pc_src;
     wire ir_write_1, ir_write_2, ir_write_3;
     wire [2:0] alu_control;
     wire zero_flag, sign_flag;
 
     datapath DP (
         .clk(clk), .reset(reset),
-        .pc_write_enable(pc_write_enable), .reg_write_enable(reg_write_enable),
+        .pc_write_enable(pc_write_enable), .reg_write_enable(reg_write_enable),.alu_src(alu_src),
         .mem_write_enable(mem_write_enable), .mem_to_reg(mem_to_reg),
-        .branch_enable(branch_enable),
+        .pc_src(pc_src),
         .ir_write_1(ir_write_1), .ir_write_2(ir_write_2), .ir_write_3(ir_write_3),
         .alu_control(alu_control),
         .zero_flag(zero_flag), .sign_flag(sign_flag),
@@ -26,9 +27,9 @@ module cpu(
         .clk(clk), .reset(reset),
         .instruction(instruction),
         .zero_flag(zero_flag), .sign_flag(sign_flag),
-        .pc_write_enable(pc_write_enable), .reg_write_enable(reg_write_enable),
+        .pc_write_enable(pc_write_enable), .reg_write_enable(reg_write_enable),.alu_src(alu_src),
         .mem_write_enable(mem_write_enable), .mem_to_reg(mem_to_reg),
-        .branch_enable(branch_enable),
+        .pc_src(pc_src),
         .ir_write_1(ir_write_1), .ir_write_2(ir_write_2), .ir_write_3(ir_write_3),
         .alu_control(alu_control)
     );
