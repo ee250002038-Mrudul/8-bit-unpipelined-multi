fb5a04ca04d181902d206bce408b2e5351eb52c0
ee250002038-Mrudul <ee250002038@iiti.ac.in>
2026-06-26 19:05:34 +0530

    Add stack pointer enable signals to datapath and control

--- cpu_top.v
+++ cpu_top.v
@@ -11,7 +11,8 @@ module cpu(
     wire ir_write_1, ir_write_2, ir_write_3;
     wire [2:0] alu_control;
     wire zero_flag, sign_flag;
-
+    wire sp_enable_add, sp_enable_sub;
+    
     datapath DP (
         .clk(clk), .reset(reset),
         .pc_write_enable(pc_write_enable), .reg_write_enable(reg_write_enable),.alu_src(alu_src),
@@ -20,7 +21,8 @@ module cpu(
         .ir_write_1(ir_write_1), .ir_write_2(ir_write_2), .ir_write_3(ir_write_3),
         .alu_control(alu_control),
         .zero_flag(zero_flag), .sign_flag(sign_flag),
-        .instruction(instruction)
+        .instruction(instruction),
+        .sp_enable_add(sp_enable_add), .sp_enable_sub(sp_enable_sub)
     );
 
     control CU (
@@ -31,7 +33,7 @@ module cpu(
         .mem_write_enable(mem_write_enable), .mem_to_reg(mem_to_reg),
         .pc_src(pc_src),
         .ir_write_1(ir_write_1), .ir_write_2(ir_write_2), .ir_write_3(ir_write_3),
-        .alu_control(alu_control)
+        .alu_control(alu_control), .sp_enable_add(sp_enable_add), .sp_enable_sub(sp_enable_sub)
     );
 
 endmodule
