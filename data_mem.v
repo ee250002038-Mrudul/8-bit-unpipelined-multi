59c7c31ac0447219c0b9fe79fc43bb22bcb97c9b
ee250002038-Mrudul <ee250002038@iiti.ac.in>
2026-06-26 19:04:18 +0530

    Update data_mem.v

--- data_mem.v
+++ data_mem.v
@@ -2,13 +2,15 @@
 
 module data_mem(
 input clk, mem_write_enable,
+input reset,
 input [7:0] address, write_data,
 output wire [7:0] read_data
-    );
+ );
     
     reg [7:0] RAM [0:255];
-    assign read_data = RAM[address];
     
+    assign read_data = RAM[address];
+        
     always @(posedge clk)
         begin
             if (mem_write_enable ==1)
